bc5de9f76de532579afbb4e491195736a75f79ab
Tomasz Stojek <stojek604@gmail.com>
2023-04-02 22:45:11 -0700

    changed dummy roles to match data dictionary

--- to-sort/Creation.sql
+++ to-sort/Creation.sql
@@ -251,16 +251,15 @@ INSERT INTO DEPARTMENT (DEP_ID, DEP_NAME) VALUES
 (2, 'Foods'),
 (3, 'Sales'),
 (4, 'Marketing'),
-(5, 'Finance');
+(5, 'Beverages');
 
 --ROLES
 INSERT INTO ROLE (ROLE_ID, ROLE_TITLE, ROLE_DESCRIPTION) VALUES
-(1, 'Manager', 'Responsible for managing the team and resources.'),
-(2, 'Supervisor', 'Oversees the work of employees and ensures quality.'),
-(3, 'Team Lead', 'Leads a team and coordinates tasks and projects.'),
-(4, 'Senior Developer', 'Develops software and mentors junior developers.'),
-(5, 'Junior Developer', 'Develops software under the guidance of seniors.'),
-(6, 'Slot Attendant', 'Manages a section of slot machines.');
+(1, 'Director of Operations', 'Responsible for managing the team and resources.'),
+(2, 'Shift Manager', 'Oversees the work of employees and ensures quality.'),
+(3, 'Floor Supervisor', 'Leads a team and coordinates tasks and floor related activiites'),
+(4, 'Slot Attendant', 'Attends to slots and other responsibilities '),
+(5, 'Human Resources', 'Protects the companys interests at all costs');
 
 -- SECTION_SKILL
 INSERT INTO SECTION_SKILL (SEC_ID, SKILL_ID) VALUES
@@ -277,16 +276,16 @@ INSERT INTO SECTION_SKILL (SEC_ID, SKILL_ID) VALUES
 
  -- EMPLOYEES
 INSERT INTO EMPLOYEE (EMP_ID, EMP_NAME, EMP_ADDRESS, EMP_DOB, EMP_GENDER, EMP_HIRE_DATE, EMP_PAY_RATE, EMP_VACATION_ENTITLEMENT, EMP_SICK_DAYS_ENTITLEMENT, DEP_ID, ROLE_ID) VALUES
-(1, 'Alice Johnson', '123 Main St', '1990-01-01', 'F', '2019-01-01', 25.00, 10, 5, 1, 6),
-(2, 'Bob Smith', '456 Oak St', '1985-02-01', 'M', '2018-02-01', 30.00, 12, 6, 2, 6),
+(1, 'Alice Johnson', '123 Main St', '1990-01-01', 'F', '2019-01-01', 25.00, 10, 5, 1, 5),
+(2, 'Bob Smith', '456 Oak St', '1985-02-01', 'M', '2018-02-01', 30.00, 12, 6, 2, 5),
 (3, 'Charlie Brown', '789 Pine St', '1992-03-01', 'M', '2017-03-01', 20.00, 8, 4, 3, 3),
-(4, 'Diana Prince', '111 Elm St', '1980-04-01', 'F', '2016-04-01', 35.00, 15, 7, 4, 6),
+(4, 'Diana Prince', '111 Elm St', '1980-04-01', 'F', '2016-04-01', 35.00, 15, 7, 4, 5),
 (5, 'Eva Green', '222 Birch St', '1988-05-01', 'F', '2015-05-01', 28.00, 11, 6, 5, 5),
-(6, 'Frank Miller', '333 Cedar St', '1995-06-01', 'M', '2014-06-01', 24.00, 9, 5, 1, 6),
+(6, 'Frank Miller', '333 Cedar St', '1995-06-01', 'M', '2014-06-01', 24.00, 9, 5, 1, 5),
 (7, 'Grace Lee', '444 Maple St', '1983-07-01', 'F', '2013-07-01', 31.00, 13, 7, 2, 3),
 (8, 'Hank Moody', '555 Walnut St', '1979-08-01', 'M', '2012-08-01', 36.00, 16, 8, 3, 4),
-(9, 'Iris West', '666 Cherry St', '1982-09-01', 'F', '2011-09-01', 29.00, 12, 6, 4, 6),
-(10, 'Jack Ryan', '777 Ash St', '1991-10-01', 'M', '2010-10-01', 22.00, 7, 4, 5, 6);
+(9, 'Iris West', '666 Cherry St', '1982-09-01', 'F', '2011-09-01', 29.00, 12, 6, 4, 5),
+(10, 'Jack Ryan', '777 Ash St', '1991-10-01', 'M', '2010-10-01', 22.00, 7, 4, 5, 5);
 
 --INTERNAL_SESSION
 --PROBLEM??????????????????????
@@ -397,7 +396,7 @@ INSERT INTO ROLE_CERT (CERT_ID, ROLE_ID) VALUES
 (4, 1),
 (4, 2),
 (5, 5),
-(5, 6);
+(5, 3);
 
 
 -----
