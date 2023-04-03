02dca848a5d0031b6dec3d5e525410b73852fea8
JVViacrusis <justin.viacrusis21p@gmail.com>
2023-04-02 23:42:09 -0700

    Remove unwanted comments and fix spacing

--- casino-queries.sql
+++ casino-queries.sql
@@ -4,8 +4,6 @@ use CasinoDB
 /*
 	QUERY ONE - Number of hours each employee worked per week
 */
-
-
 SELECT DATEPART(week, SCHEDULE.sch_date) as Week, EMPLOYEE.EMP_ID as Employee, sum( DATEPART(hour, SHIFT.shift_end) - DATEPART(hour, SHIFT.shift_start) ) as Hours
 FROM SCHEDULE
 INNER JOIN SHIFT 
@@ -19,8 +17,6 @@ ORDER BY DATEPART(week, SCHEDULE.sch_date);
 /*
 	QUERY TWO - Number of labor hours last week
 */
-
-
 SELECT DATEPART(week, SCHEDULE.sch_date) as Week, sum( DATEPART(hour, SHIFT.shift_end) - DATEPART(hour, SHIFT.shift_start) ) as Hours
 FROM SCHEDULE
 INNER JOIN SHIFT
@@ -31,8 +27,6 @@ GROUP BY DATEPART(week, SCHEDULE.sch_date);
 /*
 	QUERY THREE - List of which employees worked breaker shifts in the last month
 */
-
-
 SELECT DISTINCT EMPLOYEE.EMP_ID as "Employee ID", EMPLOYEE.EMP_NAME as "Employee Name"
 FROM EMPLOYEE
 INNER JOIN 
@@ -48,8 +42,6 @@ WHERE SHIFTS_LAST_MO.SHIFT_TYPE = 'B';
 /*
 	QUERY FOUR - How many breaker shifts are scheduled this week
 */
-
-
 SELECT COUNT(*) AS "Breaker Shifts this Week"
 FROM SHIFT sh
 INNER JOIN SCHEDULE s
@@ -61,8 +53,6 @@ AND DATEPART(week, s.SCH_DATE) = DATEPART(week, GETDATE());
 /*
 	QUERY FIVE - How many Slot Attendants are scheduled today
 */
-
-
 SELECT COUNT(*) AS "Slot Attendants Today"
 FROM EMPLOYEE e
 INNER JOIN ROLE r
@@ -77,8 +67,6 @@ WHERE r.ROLE_TITLE = 'Slot Attendant' AND s.SCH_DATE = CAST(GETDATE() as DATE);
 /*
 	QUERY SIX - Any Slot Attendants who have not been assigned to Section NORTH in the last month
 */
-
-
 SELECT DISTINCT e.EMP_ID as "Employee ID", e.EMP_NAME as "Employee Name"
 FROM EMPLOYEE e
 INNER JOIN ROLE r
@@ -101,8 +89,6 @@ WHERE r.ROLE_TITLE = 'Slot Attendant' AND e.EMP_ID NOT IN
 	This query displays all employees with active warnings. If only one employee is desired, 
 	include "AND EMP_ID=#" in the WHERE clause for desired employee.
 */
-
-
 SELECT EMP_ID as "Employee ID", COUNT(*) AS "Active Warnings"
 FROM WRITTEN_WARNING
 WHERE WW_STATUS = 1
@@ -114,8 +100,6 @@ GROUP BY EMP_ID;
 	
 	This query assumes the date sorted from least to most recent.
 */
-
-
 SELECT *
 FROM WRITTEN_WARNING
 WHERE WW_STATUS = 1
@@ -125,8 +109,6 @@ ORDER BY WW_DATE, EMP_ID;
 /*
 	QUERY NINE - A list of an employee's discipline/performance actions
 */
-
-
 SELECT EMP_ID, WW_ID, WW_DATE, WW_COMMENTS
 FROM WRITTEN_WARNING
 WHERE EMP_ID = 3;
@@ -138,8 +120,6 @@ WHERE EMP_ID = 3;
 	This query has start and end dates inclusive. This means that if an employee takes sick leave
 	from April 4 to 8, this would include both April 4 and April 8 as sick days.
 */
-
-
 SELECT e.EMP_ID as "Employee ID", e.EMP_NAME as "Employee Name", 
 	e.EMP_SICK_DAYS_ENTITLEMENT as "Sick Day Entitlement", 
 	SUM ( ISNULL( DATEDIFF(day, 
@@ -170,8 +150,6 @@ GROUP BY e.EMP_ID, e.EMP_NAME, e.EMP_SICK_DAYS_ENTITLEMENT;
 	This query has start and end dates inclusive. This means that if an employee takes vacation
 	from April 4 to 8, this would include both April 4 and April 8 as vacation days.
 */
-
-
 SELECT e.EMP_ID as "Employee ID", e.EMP_NAME as "Employee Name", 
 	e.EMP_VACATION_ENTITLEMENT as "Vacation Entitlement", 
 	SUM ( ISNULL( DATEDIFF(day, 
@@ -214,8 +192,6 @@ WHERE EMP_FIRE_DATE IS NULL AND EMP_DEPART_DATE IS NULL;
 /*
 	QUERY THIRTEEN - List of employees who has the mandatory certification expiring in the next 6 weeks
 */
-
-
 SELECT 
     e.EMP_ID, e.EMP_NAME, c.CERT_NAME, c.CERT_VALID_FOR, ts.TRAIN_DATE
 FROM 
@@ -242,10 +218,6 @@ ORDER BY
 	
 	This query only checks if employee's skill is not valid anymore.
 */
-
-
---do we want a list of employee and the skill needed to be updated? or just a list of emps that need new training, do we group by emp or skill?
-
 SELECT e.EMP_ID, e.EMP_NAME, s.SKILL_ID, s.SKILL_NAME
 FROM EMPLOYEE e
 JOIN EMPLOYEE_SKILL es ON e.EMP_ID = es.EMP_ID
@@ -256,13 +228,6 @@ WHERE es.IS_VALID = 0;
 /*
 	QUERY FIFTEEN - List of employees who have expired training
 */
-
--- ps does it make sense for valid for to be a date rather than an int representing number of months or years valid for.
--- cases emp 1 took 2 sessions for cert 1, passed both, one was 3 years ago , one was last month, not shown, PASS
--- does not take into account if employee needs a cert for their role but has never taken it,
--- does not take into account a cert that the emp has failed
--- update creation of cert and insert to be int for months instead NEEDED for this to work
-
 SELECT DISTINCT e.EMP_ID, e.EMP_NAME, c.CERT_NAME, c.CERT_ID
 FROM EMPLOYEE e
 JOIN EMP_EXTERNAL_TRAINING eet ON e.EMP_ID = eet.EMP_ID
@@ -277,9 +242,6 @@ WHERE DATEADD(MONTH, c.CERT_VALID_FOR, ts.TRAIN_DATE) < GETDATE() AND eet.IS_SUC
 	
 	This query checks the quantity of uniforms.
 */
-
---does not make use of inventory_shift junction table, revist possibly
-
 SELECT 
     (SELECT INV_QTY FROM INVENTORY WHERE INV_ID = 10) - 
     (SELECT COUNT(EMP_ID) FROM EMPLOYEE WHERE EMP_FIRE_DATE IS NULL AND EMP_DEPART_DATE IS NULL) 
